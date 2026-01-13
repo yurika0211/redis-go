8cb5684a26146e530415aba94f1f5262e073a16d
shiokou <shiokou0408@gmail.com>
2026-01-14 00:21:48 +0800

    AOF机制的完善

--- internal/persistence/aof_test.go
+++ internal/persistence/aof_test.go
@@ -7,10 +7,17 @@ import (
 
 func TestAOF(t *testing.T) {
 	fmt.Println("Testing AOF")
-	aof, err := OpenAOF("/media/shiokou/DevRepo/DevHub/Projects/2025-myapp/redis-golang/go-redis-server/log/aof.txt")
+	aof, err := OpenAOF("./log")
 	if err != nil {
 		panic(err)
 	}
-	aof.Append([]string{"SET", "foo", "bar"})
+	// 手动指定策略，解决 nil panic
+    aof.strategy = &NoStrategy{} 
+
+    // 现在执行这一行就不会崩了
+    if err := aof.Append([]string{"SET", "foo", "bar"}); err != nil {
+        panic(err)
+    }
+	
 	fmt.Println("Done")
 }
